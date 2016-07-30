9665f43f162912ce9c5bb49ec8e70733b63769c5
lukegjpotter <lukegjpotter@gmail.com>
2016-07-30 11:19:13 +0100

    JsonParsingServiceTests are passing. File creation mistake on my part.

--- app/src/main/java/com/lukegjpotter/bikeracingireland/service/json/JsonParsingService.java
+++ app/src/main/java/com/lukegjpotter/bikeracingireland/service/json/JsonParsingService.java
@@ -1,9 +1,7 @@
 package com.lukegjpotter.bikeracingireland.service.json;
 
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import com.lukegjpotter.bikeracingireland.constant.Constants;
 import com.lukegjpotter.bikeracingireland.model.BikeRace;
@@ -11,7 +9,6 @@ import com.lukegjpotter.bikeracingireland.model.BikeRace;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -24,16 +21,10 @@ class JsonParsingService {
         gsonBuilder.setDateFormat(Constants.DATE_FORMAT);
         Gson gson = gsonBuilder.create();
 
-        try { // For JSON Object - Multiple BikeRaces.
-            Type collectionType = new TypeToken<Collection<BikeRace>>() {
-            }.getType();
-            Collection<BikeRace> bikeRacesCollection = gson.fromJson(in, collectionType);
+        Type collectionType = new TypeToken<Collection<BikeRace>>() {
+        }.getType();
+        Collection<BikeRace> bikeRacesCollection = gson.fromJson(in, collectionType);
 
-            return new ArrayList<>(bikeRacesCollection);
-
-        } catch (JsonSyntaxException e) { // For JSON Array - Single BikeRace.
-            // TODO Throwing NullPointerException.
-            return Arrays.asList(gson.fromJson(in, BikeRace.class));
-        }
+        return new ArrayList<>(bikeRacesCollection);
     }
 }
