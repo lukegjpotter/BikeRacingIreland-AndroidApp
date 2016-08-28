6cd5fa0d5d6f0d0e9374126b7c5a9b3a5073d9a8
lukegjpotter <lukegjpotter@gmail.com>
2016-08-28 22:20:46 +0100

    Little refactoring of the BikeRaceDetailFragment

--- app/src/main/java/com/lukegjpotter/bikeracingireland/BikeRaceDetailFragment.java
+++ app/src/main/java/com/lukegjpotter/bikeracingireland/BikeRaceDetailFragment.java
@@ -1,8 +1,8 @@
 package com.lukegjpotter.bikeracingireland;
 
 import android.app.Activity;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,20 +13,14 @@ import com.lukegjpotter.bikeracingireland.dummy.DummyContent;
 
 /**
  * A fragment representing a single BikeRace detail screen.
- * This fragment is either contained in a {@link BikeRaceListActivity}
- * in two-pane mode (on tablets) or a {@link BikeRaceDetailActivity}
- * on handsets.
+ * This fragment is either contained in a {@link BikeRaceListActivity} in two-pane mode (on tablets)
+ * or a {@link BikeRaceDetailActivity} on handsets.
  */
 public class BikeRaceDetailFragment extends Fragment {
-    /**
-     * The fragment argument representing the item ID that this fragment
-     * represents.
-     */
+    // The fragment argument representing the item ID that this fragment represents.
     public static final String ARG_ITEM_ID = "item_id";
 
-    /**
-     * The dummy content this fragment is presenting.
-     */
+    // The dummy content this fragment is presenting.
     private DummyContent.DummyItem mItem;
 
     /**
@@ -41,9 +35,8 @@ public class BikeRaceDetailFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
+            // Load the dummy content specified by the fragment arguments. In a real-world scenario,
+            // use a Loader to load content from a content provider.
             mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
             Activity activity = this.getActivity();
@@ -55,8 +48,7 @@ public class BikeRaceDetailFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.bikerace_detail, container, false);
 
         // Show the dummy content as text in a TextView.
--- app/src/main/java/com/lukegjpotter/bikeracingireland/BikeRaceListActivity.java
+++ app/src/main/java/com/lukegjpotter/bikeracingireland/BikeRaceListActivity.java
@@ -17,19 +17,14 @@ import com.lukegjpotter.bikeracingireland.dummy.DummyContent;
 import java.util.List;
 
 /**
- * An activity representing a list of BikeRaces. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link BikeRaceDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
+ * An activity representing a list of BikeRaces. This activity has different presentations for
+ * handset and tablet-size devices. On handsets, the activity presents a list of items, which when
+ * touched, lead to a {@link BikeRaceDetailActivity} representing item details. On tablets, the
+ * activity presents the list of items and item details side-by-side using two vertical panes.
  */
 public class BikeRaceListActivity extends AppCompatActivity {
 
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
+    // Whether or not the activity is in two-pane mode, i.e. running on a tablet device.
     private boolean mTwoPane;
 
     @Override
@@ -48,7 +43,7 @@ public class BikeRaceListActivity extends AppCompatActivity {
         if (findViewById(R.id.bikerace_detail_container) != null) {
             /* The detail container view will be present only in the large-screen layouts
              * (res/values-w900dp). If this view is present, then the activity should be in
-             * two-pane mode.*/
+             * two-pane mode. */
             mTwoPane = true;
         }
     }
