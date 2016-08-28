fe76165cac728ead8e5469849c918e266114d846
lukegjpotter <lukegjpotter@gmail.com>
2016-08-28 22:07:04 +0100

    Removing the Floating Action Button from boilerplate code

--- app/src/main/java/com/lukegjpotter/bikeracingireland/BikeRaceListActivity.java
+++ app/src/main/java/com/lukegjpotter/bikeracingireland/BikeRaceListActivity.java
@@ -5,16 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 
 import com.lukegjpotter.bikeracingireland.dummy.DummyContent;
 
@@ -45,24 +41,14 @@ public class BikeRaceListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         View recyclerView = findViewById(R.id.bikerace_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
 
         if (findViewById(R.id.bikerace_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
+            /* The detail container view will be present only in the large-screen layouts
+             * (res/values-w900dp). If this view is present, then the activity should be in
+             * two-pane mode.*/
             mTwoPane = true;
         }
     }
