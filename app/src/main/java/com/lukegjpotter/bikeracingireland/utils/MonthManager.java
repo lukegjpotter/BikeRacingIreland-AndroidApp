d002d22467488639c118c0a2e6719121f3835646
lukegjpotter <lukegjpotter@gmail.com>
2016-10-05 08:46:08 +0100

    Adding a Set of Months in the List View to Month Manager

--- app/src/main/java/com/lukegjpotter/bikeracingireland/utils/MonthManager.java
+++ app/src/main/java/com/lukegjpotter/bikeracingireland/utils/MonthManager.java
@@ -1,6 +1,8 @@
 package com.lukegjpotter.bikeracingireland.utils;
 
 import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * The {@code MonthManager} class is used to handle the months in the app.
@@ -9,7 +11,13 @@ public class MonthManager {
 
     private static int sCurrentMonthNumber = Calendar.getInstance().get(Calendar.MONTH) + 1;
     private static int sListViewMonthNumber = Calendar.getInstance().get(Calendar.MONTH) + 2;
+    private static Set<Integer> sMonthsInListView = new HashSet<>();
 
+    /**
+     * Returns the Current Month Number.
+     *
+     * @return The Current Month Number.
+     */
     public static int currentMonthNumber() {
         // Update the CurrentMonth in case the user is using the app at midnight on the turn of a month.
         int monthNumberFromCalendar = Calendar.getInstance().get(Calendar.MONTH) + 1;
@@ -19,9 +27,15 @@ public class MonthManager {
             reachedBottomOfListView();
         }
 
+        sMonthsInListView.add(sCurrentMonthNumber);
+
         return sCurrentMonthNumber;
     }
 
+    /**
+     * Returns the Next Month Number.
+     * @return the Next Month Number.
+     */
     public static int nextMonthNumberForListView() {
         // Update the NextMonth in case the user is using the app at midnight on the turn of a month.
         int nextMonthNumberFromCalendar = Calendar.getInstance().get(Calendar.MONTH) + 2;
@@ -30,10 +44,21 @@ public class MonthManager {
             reachedBottomOfListView();
         }
 
+        sMonthsInListView.add(sListViewMonthNumber);
+
         return sListViewMonthNumber;
     }
 
     public static void reachedBottomOfListView() {
         sListViewMonthNumber += 1;
+    }
+
+    /**
+     * Keeps the months in use in the {@code ListView}. This is useful for the DataBase Queries.
+     *
+     * @return The months in use in the {@code ListView}.
+     */
+    public static Set<Integer> getMonthsInListView() {
+        return sMonthsInListView;
     }
 }
