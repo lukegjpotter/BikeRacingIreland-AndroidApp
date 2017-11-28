373fd5b81a04f0dcca50a5fbad2b73fb58c7c63e
lukegjpotter <lukegjpotter@gmail.com>
2017-11-28 22:33:31 +0100

    Added Queries to BikeRace and StageDetail DAOs

--- app/src/main/java/com/lukegjpotter/bikeracingireland/model/dao/BikeRaceDao.java
+++ app/src/main/java/com/lukegjpotter/bikeracingireland/model/dao/BikeRaceDao.java
@@ -1,17 +1,32 @@
 package com.lukegjpotter.bikeracingireland.model.dao;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
+import com.lukegjpotter.bikeracingireland.enums.RaceType;
 import com.lukegjpotter.bikeracingireland.model.entity.BikeRaceEntity;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by lukegjpotter on 27/11/2017.
  */
 @Dao
 public interface BikeRaceDao {
+
+    @Query("SELECT * FROM bikeraceentity WHERE id = :id")
+    LiveData<BikeRaceEntity> findBikeRaceById(long id);
+
+    @Query("SELECT * FROM bikeraceentity WHERE monthNumber = :monthNumber")
+    LiveData<List<BikeRaceEntity>> findBikeRacesInMonth(int monthNumber);
+
+    @Query("SELECT * FROM bikeraceentity, stagedetailentity WHERE raceType IN (:raceTypes) AND category IN (:categories) AND monthNumber IN (:searchMonths)")
+    LiveData<List<BikeRaceEntity>> findBikeRacesWithRaceTypeInCategoryForMonths(Set<RaceType> raceTypes, Set<String> categories, Set<Integer> searchMonths);
 
     @Insert
     void insertBikeRaces(BikeRaceEntity... BikeRaces);
--- app/src/main/java/com/lukegjpotter/bikeracingireland/model/dao/StageDetailDao.java
+++ app/src/main/java/com/lukegjpotter/bikeracingireland/model/dao/StageDetailDao.java
@@ -1,17 +1,29 @@
 package com.lukegjpotter.bikeracingireland.model.dao;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
+import com.lukegjpotter.bikeracingireland.enums.RaceType;
 import com.lukegjpotter.bikeracingireland.model.entity.StageDetailEntity;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by lukegjpotter on 27/11/2017.
  */
 @Dao
 public interface StageDetailDao {
+
+    @Query("SELECT * FROM stagedetailentity WHERE id = :id")
+    LiveData<StageDetailEntity> findStageDetailById(long id);
+
+    @Query("SELECT * FROM stagedetailentity WHERE raceType IN (:raceTypes) AND category IN (:categories)")
+    LiveData<List<StageDetailEntity>> findStageDetailsByRaceTypesAndCategories(Set<RaceType> raceTypes, Set<String> categories);
 
     @Insert
     void insertStageDetails(StageDetailEntity... StageDetails);
