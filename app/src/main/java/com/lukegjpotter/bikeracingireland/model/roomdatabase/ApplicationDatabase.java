e185ca8c06c24401953d360b495c63c491df8581
lukegjpotter <lukegjpotter@gmail.com>
2017-11-29 22:44:41 +0100

    Adding Entities to the Database

--- app/src/main/java/com/lukegjpotter/bikeracingireland/model/roomdatabase/ApplicationDatabase.java
+++ app/src/main/java/com/lukegjpotter/bikeracingireland/model/roomdatabase/ApplicationDatabase.java
@@ -10,12 +10,14 @@ import com.lukegjpotter.bikeracingireland.model.dao.BikeRaceDao;
 import com.lukegjpotter.bikeracingireland.model.dao.ProfileFilterDao;
 import com.lukegjpotter.bikeracingireland.model.dao.StageDetailDao;
 import com.lukegjpotter.bikeracingireland.model.entity.BikeRaceEntity;
+import com.lukegjpotter.bikeracingireland.model.entity.ProfileFilterEntity;
+import com.lukegjpotter.bikeracingireland.model.entity.StageDetailEntity;
 import com.lukegjpotter.bikeracingireland.model.roomdatabase.util.DateConverters;
 
 /**
  * Created by lukegjpotter on 27/11/2017.
  */
-@Database(entities = {BikeRaceEntity.class}, version = 1, exportSchema = false)
+@Database(entities = {BikeRaceEntity.class, StageDetailEntity.class, ProfileFilterEntity.class}, version = 1, exportSchema = false)
 @TypeConverters({DateConverters.class})
 public abstract class ApplicationDatabase extends RoomDatabase {
 
