3e542896221bb0cb5c6cdee7784fe8cf15ca925a
lukegjpotter <lukegjpotter@gmail.com>
2017-12-21 23:11:42 +0000

    Removing Android Runtime Permissions. Issue #3 no longer relevant.
    The INTERNET permission doesn't need to use the Runtime Permissions, as it is a Normal permission.

--- app/src/main/java/com/lukegjpotter/bikeracingireland/view/activity/BikeRaceListActivity.java
+++ app/src/main/java/com/lukegjpotter/bikeracingireland/view/activity/BikeRaceListActivity.java
@@ -1,14 +1,8 @@
 package com.lukegjpotter.bikeracingireland.view.activity;
 
-import android.Manifest;
 import android.arch.lifecycle.ViewModelProviders;
-import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,7 +11,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.lukegjpotter.bikeracingireland.R;
-import com.lukegjpotter.bikeracingireland.constant.Constants;
 import com.lukegjpotter.bikeracingireland.databinding.ActivityBikeraceListBinding;
 import com.lukegjpotter.bikeracingireland.model.entity.ProfileFilterEntity;
 import com.lukegjpotter.bikeracingireland.model.roomdatabase.ApplicationDatabase;
@@ -43,20 +36,16 @@ public class BikeRaceListActivity extends AppCompatActivity {
     private BikeRaceListViewModel bikeRaceListViewModel;
     // Whether or not the activity is in two-pane mode, i.e. running on a tablet device.
     private boolean mTwoPane;
-    private ActivityBikeraceListBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Android RunTimePermissions Request.
-        setupAndroidRunTimePermissionsAndDependencies();
-
         // The detail container view will be present only in the large-screen layouts (res/values-w900dp).
         // If this view is present, then the activity should be in two-pane mode.
         if (findViewById(R.id.bikerace_detail_container) != null) mTwoPane = true;
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_bikerace_list);
+        ActivityBikeraceListBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_bikerace_list);
 
         setSupportActionBar(binding.toolbar);
         binding.toolbar.setTitle(getTitle());
@@ -74,58 +63,15 @@ public class BikeRaceListActivity extends AppCompatActivity {
         // Insert Initial Data for testing the app.
         DatabaseInitializer.populateAsync(ApplicationDatabase.getInstance(getApplicationContext()));
 
+        // TODO: Replace with Dagger2. Initialise Retrofit.
+        BikeRaceRetrofitRepository.getInstance();
+
         // Setup ProfileFilterViewModel.
         profileFilterViewModel = ViewModelProviders.of(this).get(ProfileFilterViewModel.class);
         profileFilterViewModel.getProfileFilter().observe(this, profileFilterEntity -> profileFilterEntity = new ProfileFilterEntity(profileFilterEntity));
 
         bikeRaceListViewModel = ViewModelProviders.of(this).get(BikeRaceListViewModel.class);
         bikeRaceListViewModel.getBikeRaces().observe(this, recyclerViewAdapter::setBikeRaces);
-    }
-
-    private void setupAndroidRunTimePermissionsAndDependencies() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
-            // GRANTED: Permission to access the INTERNET, initialize Retrofit.
-            BikeRaceRetrofitRepository.getInstance();
-        } else {
-            // REQUEST: Permission to access the INTERNET.
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)) {
-                // DENIED: State why this app needs the Internet permission.
-                Snackbar.make(binding.frameLayout,
-                        "Internet access is required to get Bike Race Data.",
-                        Snackbar.LENGTH_INDEFINITE)
-                        .setAction("OK", view -> {
-                            // Request the permission
-                            ActivityCompat.requestPermissions(this,
-                                    new String[]{Manifest.permission.INTERNET},
-                                    Constants.MY_PERMISSIONS_INTERNET);
-                        }).show();
-            } else {
-                // REQUEST: First Time.
-                Snackbar.make(binding.frameLayout,
-                        "Permission is not available. Requesting Internet permission.",
-                        Snackbar.LENGTH_SHORT)
-                        .show();
-
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, Constants.MY_PERMISSIONS_INTERNET);
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == Constants.MY_PERMISSIONS_INTERNET) {
-            // Request for internet permission.
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission has been granted. Initialze Retrofit.
-                BikeRaceRetrofitRepository.getInstance();
-            } else {
-                // Permission request was denied.
-                Snackbar.make(binding.frameLayout,
-                        "Internet permission request was denied.",
-                        Snackbar.LENGTH_SHORT)
-                        .show();
-            }
-        }
     }
 
     @Override
